780170a28bcb887e21a8f2c6fe26a672ff39e51d
Jacob Yuill <jacobyuill@icloud.com>
2025-07-31 16:30:07 -0500

    Initial commit

---
+++ jacobtimer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sec_to_min() {
+    if [[ $# -ne 1 ]]; then
+        echo "Error: Used more than one arg"
+        exit 1;
+    fi
+    
+    local minutes=$(($1 / 60))
+    local seconds=$(($1 % 60 ))
+    printf "%d:%02d" "$minutes" "$seconds"
+}
+
+main() {
+    read -p "Enter timer length (min): " time
+    (( time *= 60 ))
+    
+    for ((i = 0; i <= time; i++)); do
+        printf "\r%s | %s" "$(sec_to_min $i)" "$(sec_to_min $time)"
+        sleep 1 
+    done
+
+    printf "\nTime's up!"
+
+    # repeat 5 times as an "alarm"
+    for i in {5..1}; do
+        paplay ./alarm.ogg
+    done
+}
+
+while true; do 
+  main # call original program 
+  printf "\nPress r to restart, or q to quit."
+  read -n 1 -s key
+    case "$key" in 
+      r|R)
+        #restarts automatically
+        ;;
+      q|Q)
+        printf "\n"
+        exit 1;
+        ;;
+    esac
+done
+
